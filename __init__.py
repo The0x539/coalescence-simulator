6cfc1dd1c0225bc777a9d4cacff6678d2936056d
The0x539 <the0x539@gmail.com>
2021-12-01 18:34:02 -0600

    better types n stuff

--- __init__.py
+++ __init__.py
@@ -1,13 +1,11 @@
 from uuid import UUID, uuid4
 import math
-from typing import List, Set, Optional, Tuple, Dict
+from typing import List, Set, Optional, Tuple, Dict, TypeVar, Type, cast
 from abc import ABC, abstractmethod
 from enum import Enum, auto
 
 
-class EntityType(Enum):
-    NODE = auto()
-    DEVICE = auto()
+T = TypeVar("T")
 
 
 class RunningTask:
@@ -45,19 +43,6 @@ class Entity(ABC):
     @abstractmethod
     def tick(self, world: World) -> None:
         ...
-
-    @abstractmethod
-    def entity_type(self) -> EntityType:
-        ...
-
-    def estimate_time(self, task: Task) -> int:
-        assert False
-
-    def spawn(self, task: Task) -> None:
-        assert False
-
-    def get_results(self, id: UUID) -> None:
-        assert False
 
 
 class Node(Entity):
@@ -100,52 +85,47 @@ class Node(Entity):
         # can be implemented to require ticks to send the data
         pass
 
-    def entity_type(self) -> EntityType:
-        return EntityType.NODE
-
 
 class Device(Entity):
     def __init__(self, x: int, y: int, range: float) -> None:
         super().__init__(x, y, range)
-        self.tasks: List[Tuple[Task, int]] = []
+        self.tasks: List[Tuple[Task, Optional[int]]] = []
         self.runners: Dict[Task, Entity] = {}
 
     def tick(self, world: World) -> None:
         # A tick for a device is one iteration over its tasks (it does more than one tick of work in the tick, we can change it later if we want)
         indices_to_remove = []
         for i in range(0, len(self.tasks)):
-            (task, ticksLeft) = self.tasks[i]
-            if ticksLeft == -1:
+            (task, ticks_left) = self.tasks[i]
+            if ticks_left is None:
                 # task is not running anywhere
                 # tries to find a place for it to run
-                best_time = -1
-                for neighbor in world.neighbors_of(self, {EntityType.NODE}):
+                best_time = None
+                for neighbor in world.neighbors_of(self, Node):
                     time = neighbor.estimate_time(task)
-                    if time < best_time or best_time == -1:
+                    if best_time is None or time < best_time:
                         best_time = time
-                        best_node: Entity = neighbor
-                if best_time != -1:
+                        best_node = neighbor
+
+                if best_time is not None:
                     best_node.spawn(task)
                     self.tasks[i] = (task, best_time)
                     self.runners.update({task: best_node})
 
-            elif ticksLeft == 0:
+            elif ticks_left == 0:
                 # get the computation results
                 best_node.get_results(task.id)
                 indices_to_remove.append(i)
 
             else:
                 # reduce the time left
-                self.tasks[i] = (task, ticksLeft - 1)
+                self.tasks[i] = (task, ticks_left - 1)
 
-        for i in indices_to_remove:
+        for i in indices_to_remove[::-1]:
             del self.tasks[i]
 
-    def entity_type(self) -> EntityType:
-        return EntityType.DEVICE
-
     def request_task(self, cpu_work: int, gpu_work: int) -> None:
-        self.tasks.append((Task(cpu_work, gpu_work), -1))
+        self.tasks.append((Task(cpu_work, gpu_work), None))
 
 
 class World:
@@ -160,20 +140,27 @@ class World:
         self.entities.append(entity)
 
     def neighbors_of(
-        self, entity: Entity, types: Optional[Set[EntityType]] = None
-    ) -> Set[Entity]:
-        neighbors = set()
+        self,
+        entity: Entity,
+        neighbor_type: Type[T],
+    ) -> Set[T]:
+        neighbors: Set[T] = set()
         for potential_neighbor in self.entities:
             if potential_neighbor is entity:
                 continue
 
-            if types is not None and entity.entity_type() not in types:
+            # mypy what are you smoking
+            pnx = potential_neighbor.x
+            pny = potential_neighbor.y
+            pnr = potential_neighbor.range
+
+            if not isinstance(potential_neighbor, neighbor_type):
                 continue
 
-            dx = entity.x - potential_neighbor.x
-            dy = entity.y - potential_neighbor.y
+            dx = entity.x - pnx
+            dy = entity.y - pny
             distance = math.sqrt(dx * dx + dy * dy)
-            if distance > min(entity.range, potential_neighbor.range):
+            if distance > min(entity.range, pnr):
                 continue
 
             neighbors.add(potential_neighbor)
